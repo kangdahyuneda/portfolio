77563bdd33bbea6bff651999ddf99e9087ea5662
Dahyun Kang <dahyun2346@naver.com>
2022-08-09 10:44:49 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,74 @@
+$(function () {
+    function pubslider(pubslide) {
+        $(".siteSlider>ul>li").removeClass("on");
+        $(".siteSlider>ul>li").eq(pubslide).addClass("on");
+        $(".R_pub>ul>li").removeClass("on");
+        $(".R_pub>ul>li").eq(pubslide).addClass("on");
+    }
+    // 퍼블리싱 탭메뉴 함수
+    function pubtab(pubslide) {
+        $(".web_tab>ul>li").removeClass("on");
+        $(".web_tab>ul>li").eq(pubslide).addClass("on");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})//opening
