656b1a171b67cb8a1f434fc341bf2f608c0554a6
Dahyun Kang <dahyun2346@naver.com>
2022-08-09 12:41:56 +0900

    third ver

--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
 $(function () {
-    $(".siteSlider>ul>li").removeClass("on");
-    $(".siteSlider>ul>li").eq(pubslide).addClass("on");
-    $(".R_pub>ul>li").removeClass("on");
-    $(".R_pub>ul>li").eq(pubslide).addClass("on");
-}
-    // 퍼블리싱 탭메뉴 함수
-        $(".web_tab>ul>li").removeClass("on");
-$(".web_tab>ul>li").eq(pubslide).addClass("on");
-    });
+    //     $('.siteSlider>ul>li').removeClass("on");
+    //     $('.siteSlider>ul>li').eq(pubslide).addClass("on");
+    //     $(".R_pub>ul>li").removeClass("on");
+    //     $(".R_pub>ul>li").eq(pubslide).addClass("on");
 
+    //     $('.web_tab ul li').removeClass("on");
+    //     $('.web_tab ul li').eq(pubslide).addClass("on");
+    // });
+
+    $('.siteSlider>ul').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: true,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        pauseOnHover: false,
+    });
 
 
 
