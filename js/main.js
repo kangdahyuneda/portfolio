1818078fa2f610057574419b91fc1deb29ced55c
Dahyun Kang <dahyun2346@naver.com>
2022-08-10 16:26:32 +0900

    first ver

--- js/main.js
+++ js/main.js
@@ -8,18 +8,55 @@ $(function () {
     //     $('.web_tab ul li').eq(pubslide).addClass("on");
     // });
 
-    $('.siteSlider>ul').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: false,
-        arrows: true,
-        autoplay: false,
-        autoplaySpeed: 2000,
-        pauseOnHover: false,
+    // $('.siteSlider>ul').slick({
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    //     dots: false,
+    //     arrows: true,
+    //     autoplay: false,
+    //     pauseOnHover: false,
+    // });
+
+    // $('.pubsite>ul').slick({
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    //     dots: false,
+    //     arrows: true,
+    //     autoplay: false,
+    //     pauseOnHover: false,
+    // });
+
+    //퍼블리싱 탭메뉴 함수
+    $('.web_tab ul li').on('click', function () {
+        $(this).css({
+            color: '#752ea1'
+        }).siblings().css({
+            color: '#000'
+        })
+        let i = $(this).index();
+        $('.siteSlider>ul>li').eq(i).show().siblings().hide();
+        $('.Rweb_site ul li').eq(i).show().siblings().hide();
     });
 
+    $('.pub_tab ul li').on('click', function () {
+        $(this).css({
+            color: '#752ea1'
+        }).siblings().css({
+            color: '#000'
+        })
+        let i = $(this).index();
+        $('.pubsite>ul>li').eq(i).show().siblings().hide();
+        $('.Rpub_site>ul>li').eq(i).show().siblings().hide();
+    });
 
-
+    var $skillVar = $('.skill>ul>li>p>span:nth-child(2)');
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1000) {
+            $skillVar.addClass('skill_bar');
+        } else {
+            $skillVar.removeClass('skill_bar');
+        }
+    });
 
 
 
