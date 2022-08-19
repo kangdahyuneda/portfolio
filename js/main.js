2efd6bfc555971abe305807a37a1e0c159f9ef02
Dahyun Kang <dahyun2346@naver.com>
2022-08-19 12:17:32 +0900

    fin ver

--- js/main.js
+++ js/main.js
@@ -132,7 +132,16 @@ $(function () {
         $(".pub_tab>ul>li").eq(pubslide).addClass("on");
     }
 
+    //etc
+    $('#etc>ul>li').click(function (e) {
+        e.preventDefault();
+        $(this).find('.modal_etc').show();
+    });
 
+    $('.modal_etc').click(function (e) {
+        e.preventDefault();
+        $('.modal_etc').fadeOut();
+    });
 
 
 
