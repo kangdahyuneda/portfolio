e1e927a8bf507ce58e82e33f0fab9a2347dd9cea
Dahyun Kang <dahyun2346@naver.com>
2022-08-23 12:05:25 +0900

    jeju modify

---
+++ portfolio/jeju/js/main.js
@@ -0,0 +1,70 @@
+$(function () {
+    $('.maintxt').slick({
+        slidesToShow: 1, // 화면에 보여지는 이미지 갯수
+        slidesToScroll: 1, // 스크롤시 이동할 이미지 갯수
+        arrows: true, // 화살표 사용 여부 
+        dots: false, // 페이저 사용 여부
+        speed: 1000, // 다음 페이지로 넘어가는 시간
+        autoplay: true, //자동재생 여부
+        autoplaySpeed: 1000, // 자동재생 시 이미지가 유지되는 시간
+        pauseOnHover: true, //마우스 hover시 자동재생 멈춤 여부
+        infinite: true, // 무한반복 여부
+    });
+
+    $('.cont2in').slick({
+        slidesToShow: 4, // 화면에 보여지는 이미지 갯수
+        slidesToScroll: 1, // 스크롤시 이동할 이미지 갯수
+        arrows: true, // 화살표 사용 여부 
+        dots: false, // 페이저 사용 여부
+        speed: 1000, // 다음 페이지로 넘어가는 시간
+        autoplay: true, //자동재생 여부
+        autoplaySpeed: 1000, // 자동재생 시 이미지가 유지되는 시간
+        pauseOnHover: true, //마우스 hover시 자동재생 멈춤 여부
+        infinite: true, // 무한반복 여부
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})//opening
