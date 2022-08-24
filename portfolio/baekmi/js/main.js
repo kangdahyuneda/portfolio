d5ccf602db5a09c86e81eddae9da1b97806ab239
Dahyun Kang <dahyun2346@naver.com>
2022-08-24 17:46:00 +0900

    baekmi modify

--- portfolio/baekmi/js/main.js
+++ portfolio/baekmi/js/main.js
@@ -12,22 +12,32 @@ $(function () {
         vertical: true, // 세로 슬라이드
     });
 
+    /* $('.t_img').slick({
+        slidesToShow: 1, // 화면에 보여지는 이미지 갯수
+        slidesToScroll: 1, // 스크롤시 이동할 이미지 갯수
+        arrows: true, // 화살표 사용 여부 
+        dots: false, // 페이저 사용 여부
+        speed: 1000, // 다음 페이지로 넘어가는 시간
+        autoplay: true, //자동재생 여부
+        autoplaySpeed: 1000, // 자동재생 시 이미지가 유지되는 시간
+        pauseOnHover: true, //마우스 hover시 자동재생 멈춤 여부
+        infinite: false, // 무한반복 여부
+    }); */
 
     $('.t_img').slick({
+        arrows: true,
         slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: false,
-        arrows: false,
+        // slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 2000,
-        pauseOnHover: false,
+        autoplaySpeed: 1000,
     });
 
     $('.sl_img').slick({
-        prevArrow: $('.prv'),
-        nextArrow: $('.nxt'),
+        arrows: false,
+        slidesToShow: 1,
+        // slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 1000,
+        autoplaySpeed: 3000,
     });
 
     $('.btnMenu').on('click', function () {
